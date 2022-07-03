91adde64cee4fb5c23bb8f7fff7af5051d294d00
Your Name <you@example.com>
2022-07-03 22:15:01 +0200

    modif page paints

--- src/Controller/PaintsController.php
+++ src/Controller/PaintsController.php
@@ -7,14 +7,23 @@ use App\Repository\PaintsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 
 class PaintsController extends AbstractController
 {
     #[Route('/paints', name: 'app_paints')]
-    public function index(PaintsRepository $paintsRepository): Response
+    public function index(PaintsRepository $paintsRepository, PaginatorInterface $paginatorInterface, Request $request): Response
     {
+        $paints = $paginatorInterface->paginate(
+            $paintsRepository->findAll(),
+            $request->query->getInt('page', 1),
+            5
+        );
+    
+
         return $this->render('paints/index.html.twig', [
-            'paints' => $paintsRepository->findAll(),
+            'paints' => $paints
             
         ]);
     }
