dd389861b7cf8f4209dfadec4f97a42957c1597f
Your Name <you@example.com>
2022-07-27 16:47:09 +0200

    modif base category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,18 +3,37 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Paints;
 use App\Repository\CategoryRepository;
+use App\Repository\PaintsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
-    public function index(Categoryrepository $categoryrepository): Response
+    public function index(CategoryRepository $categoryRepository,PaintsRepository $paintsRepository , PaginatorInterface $paginatorInterface ,Request $request): Response
     {
+
+        $id = $request->query->get("id");
+        $category = $categoryRepository->findBy(['paints' => $id]);
+        $paints = $paintsRepository->findBy(['category' =>  $id]);
+
+        if ($id) {
+            $idcategory = $categoryRepository->find($id);
+        }  else {
+            $paints = $paginatorInterface->paginate(
+                $paintsRepository->findBy(['category' => $id]),
+                $request->query->getInt('page', 1),
+                10
+            );
+        }
         return $this->render('category/index.html.twig', [
-            'category' => $categoryrepository->findAll()
+            'category' => $category,
+            'paints'   => $paints,
         ]);
     }
 }
